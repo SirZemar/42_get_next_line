a74b6b00b33b7cbbb846f9514f21b4dff58eecfa
SirZemar <eduardo.w.marinho@gmail.com>
2023-11-02 23:25:09 +0000

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-ero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 21:27:21 by jose-ero          #+#    #+#             */
+/*   Updated: 2023/11/02 21:27:22 by jose-ero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_find_newline(char *ptr);
+size_t	ft_strlen(const char *s);
+char	*ft_join_stash(char *stash, char *buffer);
+char	*get_next_line(int fd);
+
+#endif
